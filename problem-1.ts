2c1b6b9b6c2385631b0f1f2d237df320db1d59df
riyaz-369 <riyazulislam0099@gmail.com>
2024-11-08 18:41:03 +0600

    solved: problem-1

--- problem-1.ts
+++ problem-1.ts
@@ -1,3 +1,7 @@
-const naem: string = "john";
-
-console.log(naem);
+const sumArray = (numArr: number[]): number => {
+  let sum: number = 0;
+  for (let i: number = 0; i < numArr.length; i++) {
+    sum = sum + numArr[i];
+  }
+  return sum;
+};
